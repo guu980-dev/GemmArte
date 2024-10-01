89de0801fdd41f3b3e0f80e35cda48e429c71e87
guu980-dev <guudev980@gmail.com>
2024-10-01 02:21:30 +0000

    feat: fine tune composition data

--- test_inference.py
+++ test_inference.py
@@ -5,16 +5,20 @@ from PIL import Image
 def main():
   model_id = "google/paligemma-3b-pt-224"
   # model_path = "output/1727488022/checkpoint-112"
-  model_path = "output/1727490265/checkpoint-450"
+  # model_path = "output/1727490265/checkpoint-450"
+  model_path = "output/1727643637/checkpoint-4760"
   model = PaliGemmaForConditionalGeneration.from_pretrained(model_path)
   processor = AutoProcessor.from_pretrained(model_id)
 
   # prompt = "Analyze image from a critic's point of view."
-  prompt = "Please construct a formal analysis paragraph that is coherent and focuses solely on visual characteristic."
+  # prompt = "Please construct a formal analysis paragraph that is coherent and focuses solely on visual characteristic."
+  # prompt = "Conduct a visual analysis that emphasizes the compositional elements, examining the arrangement and structural balance of the artwork.
+  # prompt = "Conduct a visual analysis in one paragraph with multiple sentences that emphasizes the compositional elements, examining the arrangement and structural balance of the artwork."
+  prompt = "Conduct a detailed visual analysis step by step that emphasizes the compositional elements, examining the arrangement and structural balance of the artwork."
   image_file_path = "dataset/images/manual_test/starry_night.jpg"
   raw_image = Image.open(image_file_path)
   inputs = processor(prompt, raw_image, return_tensors="pt")
-  output = model.generate(**inputs, max_new_tokens=20)
+  output = model.generate(**inputs, max_new_tokens=200)
 
   # Starry Night
   print("Response: ", processor.decode(output[0], skip_special_tokens=True)[len(prompt):])
